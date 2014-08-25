34016bb498ecce12721ad365960f6f498715ea06
Dylan Peerenboom <peerend@gmail.com>
2014-08-25 15:03:59 -0700

    includes bootstrap navbar formatting and required js files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,15 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
+
+(function() {
+ var nav = document.getElementById('nav'),
+ anchor = nav.getElementsByTagName('a'),
+ current = window.location.pathname.split('/')[1];
+ for (var i = 0; i < anchor.length; i++) {
+ if(anchor[i].href == current) {
+ anchor[i].className = "active";
+ }
+ }
+})();
